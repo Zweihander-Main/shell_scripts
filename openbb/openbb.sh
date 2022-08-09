6ae1340cc8d17d4a690a8f120e99d2bed50e2980
Zweihänder <zweidev@zweihander.me>
2022-08-08 22:41:53 -0400

    fix(openbb): format docker command

--- openbb/openbb.sh
+++ openbb/openbb.sh
@@ -3,5 +3,10 @@
 # Scenario: You want to run OpenBBTerminal via docker on Linux with X support.
 
 xhost +local:
-docker run -it --rm --name openbb --env-file="${XDG_CONFIG_HOME}/openbb/openbb.env" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix ghcr.io/openbb-finance/openbbterminal-poetry:latest
+sudo docker run -it --rm \
+	--name openbb \
+	--env-file="${XDG_CONFIG_HOME}/openbb/openbb.env" \
+       	-e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	ghcr.io/openbb-finance/openbbterminal-poetry:latest
 xhost -local:
