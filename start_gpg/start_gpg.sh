c19da6181a02a9750e73b3efecab55041d124770
Zweihänder <zweidev@zweihander.me>
2023-11-11 22:11:35 -0500

    fix(start_gpg): ensure dir exists

--- start_gpg/start_gpg.sh
+++ start_gpg/start_gpg.sh
@@ -2,5 +2,10 @@
 
 # Scenario: start GPG, connecting to right TTY
 
-export GPG_TTY=$(tty)
+if [ ! -d "$GNUPGHOME" ]; then
+    mkdir -p "$GNUPGHOME"
+fi
+
+GPG_TTY=$(tty)
+export GPG_TTY
 gpg-connect-agent updatestartuptty /bye >/dev/null
